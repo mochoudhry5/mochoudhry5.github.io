8ba6cd0a07f9818bc9d73c95e4a0b178c53a6cf4
Momin Choudhry <Mochoudhry5@gmail.com>
2022-04-13 01:09:03 -0700

    first commit

--- assignment4/fancifymytext.js
+++ assignment4/fancifymytext.js
@@ -1,31 +1,26 @@
 function bigger() {
   document.getElementById("textarea").style.fontSize = "24pt";
-  alert("Hello World")
 }
 
 function fancy() {
   let userText = document.getElementById("textarea");
-  if (document.getElementById("fancytext").checked) {
-    document.getElementById("boringText").checked = false;
-    userText.style.fontWeight = "bold";
-    userText.style.textDecoration = "underline";
-    userText.style.color = "blue";
-  }
+  document.getElementById("boringText").checked = false;
+  userText.style.fontWeight = "bold";
+  userText.style.textDecoration = "underline";
+  userText.style.color = "blue";
 }
 
 function boringBetty() {
   let userText = document.getElementById("textarea");
-  if (document.getElementById("boringText").checked) {
-    document.getElementById("fancytext").checked = false;
-    userText.style.fontWeight = "normal";
-    userText.style.textDecoration = "none";
-    userText.style.color = "black";
-  }
+  document.getElementById("fancytext").checked = false;
+  userText.style.fontWeight = "normal";
+  userText.style.textDecoration = "none";
+  userText.style.color = "black";
 }
 
 function moo() {
   let userText = document.getElementById("textarea");
   userText.style.textTransform = "uppercase";
   let split = userText.value.split(".");
-  userText.value = split.join("-Moo.");
+  userText.value = split.join("-Moo");
 }
